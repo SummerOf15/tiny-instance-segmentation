647e00cc76f4f13e351b49444f44a0a29d33edd5
summerof15 <zhangkai11@11.com>
2021-04-16 18:59:37 +0200

    first version

--- generate_txt.py
+++ generate_txt.py
@@ -3,7 +3,7 @@ This code aims to generate the train.txt and test.txt
 
 """
 import random
-
+import os
 
 
 def compare_txt():
@@ -28,9 +28,9 @@ def compare_txt():
 def generate_txt():
     random.seed(1)
 
-    all_file=open("ImageSets/Main/all.txt","r").readlines()
-    test_file=open("ImageSets/test.txt","w")
-    train_file=open("ImageSets/train.txt","w")
+    all_file=open("dataset/ImageSets/Main/all.txt","r").readlines()
+    test_file=open("dataset/ImageSets/Main/test.txt","w")
+    train_file=open("dataset/ImageSets/Main/train.txt","w")
 
     test=random.sample(all_file,45)
     train=[x for x in all_file if x not in test]
@@ -40,7 +40,16 @@ def generate_txt():
     test_file.close()
     train_file.close()
     
+def generate_all_txt():
+    annotation_files=os.listdir("dataset/Annotations/")
+    all_file=open("dataset/ImageSets/Main/all.txt","w")
+
+    annotation_files=[x[:-4] for x in annotation_files if x.endswith("xml")]
+    all_file.write("\n".join(annotation_files))
+    all_file.close()
+
 
 if __name__ == "__main__":
+    generate_all_txt()
     generate_txt()
 
--- scripts/tuftrecognition/tuft_rcnn_resnet50_fpn.sh
+++ scripts/tuftrecognition/tuft_rcnn_resnet50_fpn.sh
@@ -1,9 +1,30 @@
 #!/bin/sh
 
-source /volume/USERSTORE/lee_jn/miniconda3/bin/activate p_detector
+conda activate tor
+
+echo "=====Job Infos ===="
+# echo "Node List: " $SLURM_NODELIST
+# echo "jobID: " $SLURM_JOB_ID
+# echo "Partition: " $SLURM_JOB_PARTITION
+# echo "Submit directory:" $SLURM_SUBMIT_DIR
+# echo "Submit host:" $SLURM_SUBMIT_HOST
+echo "In the directory: `pwd`"
+echo "As the user: `whoami`"
+echo "Python version: `python -c 'import sys; print(sys.version)'`"
+echo "pip version: `pip --version`"
+
 
 export CUDA_HOME=/
 
-DIR=/volume/USERSTORE/lee_jn/p_detector
+DIR=/media/ck/B6DAFDC2DAFD7F45/program/pyTuft/tiny-instance-segmentation
 
-TORCH_HOME=$DIR ; python $DIR/tools/tuft_detection.py --num-gpus 1 --dist-url tcp://127.0.0.1:52114 --resume --config-file $DIR/configs/haicu/faster_rcnn_resnet50_fpn.yaml SOLVER.IMS_PER_BATCH 2 SOLVER.BASE_LR 0.0025 OUTPUT_DIR /home_local/lee_jn/experiments/haicu/res50fpn DATA_DIR /home_local/lee_jn/data_tuft
+start_time=`date +%s`
+echo "Job Started at "`date`
+
+TORCH_HOME=$DIR ; python $DIR/tools/tuft_detection.py --num-gpus 1 --dist-url tcp://127.0.0.1:52114 --resume --config-file $DIR/configs/haicu/faster_rcnn_resnet50_fpn.yaml SOLVER.IMS_PER_BATCH 2 SOLVER.BASE_LR 0.0025 OUTPUT_DIR $DIR/experiments/haicu/res50fpn DATA_DIR $DIR/dataset
+
+echo "Job ended at "`date`
+end_time=`date +%s`
+total_time=$((end_time-start_time))
+
+echo "Took " $total_time " s"
